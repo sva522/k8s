640f20878ff812103d4ad61f0f1e4d810dcf9abc
sva522 <nobody@nomail.org>
2026-01-16 21:41:50 +0100

    OK

--- cluster/restore_state.sh
+++ cluster/restore_state.sh
@@ -8,9 +8,9 @@ if [ ! -v 1 ]; then
     echo 'Missing snapthot name' && exit 1
 fi
 
-virsh destroy k8s1 2>/dev/null
-virsh destroy k8s2 2>/dev/null
-virsh destroy k8s3 2>/dev/null
+virsh -c qemu:///system destroy k8s1 2>/dev/null
+virsh -c qemu:///system destroy k8s2 2>/dev/null
+virsh -c qemu:///system destroy k8s3 2>/dev/null
 
 restore_snapshot(){
     vm_name="$1"
@@ -22,9 +22,13 @@ restore_snapshot k8s2 "$1" &
 restore_snapshot k8s3 "$1" & 
 wait
 
-wait_for_ssh k8s1
-wait_for_ssh k8s2
-wait_for_ssh k8s3
+readonly k8s1_ip=$(dig k8s1 +short "@$dns_vm_admin")
+readonly k8s2_ip=$(dig k8s2 +short "@$dns_vm_admin")
+readonly k8s3_ip=$(dig k8s3 +short "@$dns_vm_admin")
+
+wait_for_ssh k8s1 "$k8s1_ip"
+wait_for_ssh k8s2 "$k8s2_ip"
+wait_for_ssh k8s3 "$k8s3_ip"
 ssh admin@k8s1 wait_for_ntp
 ssh admin@k8s2 wait_for_ntp
 ssh admin@k8s3 wait_for_ntp
