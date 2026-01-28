16e5b5761a7e35373aa8e598d1d2243261cfa211
No Body <no@one.com>
2026-01-28 10:27:44 +0100

    OK

--- cluster/sav_state.sh
+++ cluster/sav_state.sh
@@ -8,7 +8,7 @@ fi
 create_snapshot(){
     vm_name="$1"
     snapshot_name="$2"
-    virsh -c qemu:///system snapshot-create-as --domain "$vm_name" --name "$snapshot_name" --live # --description 'Before update'
+    virsh -c qemu:///system snapshot-create-as --domain "$vm_name" --name "$snapshot_name" # --description 'Before update'
 }
 
 create_snapshot k8s1 "$name" --live &
